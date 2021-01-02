cc2dd5db0ec8bb3e5b1340c37be4f6e95d487e0f
Denis Compov <dollarz@my.my>
2021-01-02 08:39:12 +0600

    add class Arabic

--- src/Test_0/Arabic.java
+++ src/Test_0/Arabic.java
@@ -1,1 +1,43 @@
 package Test_0;
+
+public class Arabic {
+
+    public static void PrintResult(int number1, char sign, int number2){
+        switch (sign){
+            case '+':
+                System.out.println(number1+number2);
+                break;
+            case '-':
+                System.out.println(number1-number2);
+                break;
+            case '*':
+                System.out.println(number1*number2);
+                break;
+            case '/':
+                try {
+                    System.out.println(number1/number2);
+                } catch (Exception e) {
+                    System.out.println("DivByZero Exception: " + e.getMessage());
+                    System.exit(-1);
+                }
+
+                break;
+        }
+    }
+    public static Number getArabicNumberFromString(String strNumber){
+        Number n = new Number();
+        try{
+            n.value = Integer.parseInt(strNumber);
+            n.geographyNumber=true;
+        }catch (NumberFormatException e){
+            try {
+                n.value=Roman.ParseInt(strNumber);
+                n.geographyNumber=false;
+            }catch (NumberFormatException e2){
+                System.out.println("NumberFormatException: " + e.getMessage() + " "+e2.getMessage());
+                System.exit(-1);
+            }
+        }
+        return n;
+    }
+}
--- src/Test_0/InputLineToNumbers.java
+++ src/Test_0/InputLineToNumbers.java
@@ -1,2 +1,21 @@
 package Test_0;
 
+
+public class InputLineToNumbers {
+
+    public static Number Number1;
+    public static Number Number2;
+
+    public InputLineToNumbers(){
+
+        Number1=Arabic.getArabicNumberFromString(InputLineToStrNumbers.strNumber1);
+        Number2=Arabic.getArabicNumberFromString(InputLineToStrNumbers.strNumber2);
+        try {
+            if ((Number1.value<1)|(Number1.value>10)) throw new NumberFormatException("Число вне диапазона");
+            if ((Number2.value<1)|(Number2.value>10)) throw new NumberFormatException("Число вне диапазона");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            System.exit(-2);
+        }
+    }
+}
